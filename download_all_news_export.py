5ea3b1b35399e379ea3aa4cbb057accdca56fa16
AndrewPang <thpangab@connect.ust.hk>
2018-01-23 13:37:58 +0800

    Added all_categories & main_category for each article and filtered out meaningless news

--- download_all_news_export.py
+++ download_all_news_export.py
@@ -1,3 +1,11 @@
+import argparse
+import io
+import os
+
+from google.cloud import language
+import numpy
+import six
+
 import newspaper
 import json
 import datetime
@@ -41,6 +49,11 @@ def getAllNewsExport(paper, all_sub_urls, paper_info):
             # nlp to get keywords and summary   
             article.nlp()
 
+            # Ignore article which is not a news (with text < 500) crawled from the web
+            if len(article.text) < 500:
+                print(article.title, "is skipped as not enough content.")
+                continue
+
             # create a dictionary to store the news data ('images' attribute may be in wrong format in the parsing process)
             news = {}
             news['url'] = article.url
@@ -57,6 +70,16 @@ def getAllNewsExport(paper, all_sub_urls, paper_info):
             news['movies'] = article.movies
             news['keywords'] = article.keywords
             news['summary'] = article.summary
+
+            # Category Classification using Google Cloud NLP API
+            categories = classify(article.text, False)
+            for category in categories:
+                categories[category.replace("/", "_")] = categories.pop(category)
+
+            news['all_categories'] = categories
+            # print("MAIN CATEGORY: ", max(categories, key=categories.get))
+            news['main_category'] = max(categories, key=categories.get)
+
             # append the dict object into the list
             all_articles_details.append(news)
             # TIPS: use json.dumps(WHOLE_LIST) to change all single quotes of dict to double quotes of JSON format
@@ -73,6 +96,34 @@ def getAllNewsExport(paper, all_sub_urls, paper_info):
     fo.write(paper_info_formatted)
     # Close opened file
     fo.close()
+
+def classify(text, verbose=True):
+    """Classify the input text into categories. """
+
+    language_client = language.LanguageServiceClient()
+
+    document = language.types.Document(
+        content=text,
+        type=language.enums.Document.Type.PLAIN_TEXT)
+    response = language_client.classify_text(document)
+    categories = response.categories
+
+    result = {}
+
+    for category in categories:
+        # Turn the categories into a dictionary of the form:
+        # {category.name: category.confidence}, so that they can
+        # be treated as a sparse vector.
+        result[category.name] = category.confidence
+
+    if verbose:
+        print(text)
+        for category in categories:
+            print(u'=' * 20)
+            print(u'{:<16}: {}'.format('category', category.name))
+            print(u'{:<16}: {}'.format('confidence', category.confidence))
+
+    return result
 
 ### Main Program ###
 # url = "http://edition.cnn.com/"
