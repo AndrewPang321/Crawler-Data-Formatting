2e72f2bed641da5a3bf14f3d01915a55b6a3e6ad
AndrewPang <thpangab@connect.ust.hk>
2018-03-23 11:59:27 +0800

    Modify the program to output the results as csv

--- download_movies_dataset_tmdb.py
+++ download_movies_dataset_tmdb.py
@@ -1,12 +1,26 @@
+import io
+import datetime
+import time
+import json
+import csv
 import http.client
 
 conn = http.client.HTTPSConnection("api.themoviedb.org")
 
 payload = "{}"
 
-conn.request("GET", "/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=a6ffbc3c2a4dd66577851418f32b6da0", payload)
+fo = csv.writer(open("all_movies_" + str(datetime.datetime.now()).replace(" ", "_") + ".txt", "a"))
 
-res = conn.getresponse()
-data = res.read()
+for i in range(1000):
+    ### 20 movies for each page ###
+    print("Running page " + str(i+1) + " ...")
+    conn.request("GET", "/3/discover/movie?page=" + str(i+1) + "&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=a6ffbc3c2a4dd66577851418f32b6da0", payload)
+    res = conn.getresponse()
+    data = res.read()
+    data_json = json.loads(data)
 
-print(data.decode("utf-8"))
+    # write to csv file
+    for item in data_json["results"]:
+        fo.writerow([item["id"], item["title"], item["vote_average"], "https://image.tmdb.org/t/p/w500"+item["poster_path"]])
+
+    time.sleep(1)
